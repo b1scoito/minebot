e5681a7ba99ca37fe5c2078836e781b87205f6b3
b1scoito <rafaelbadas123@gmail.com>
2018-09-09 06:37:38 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-var mineflayer = require('mineflayer');
-var names = require('./names.json');
-if (process.argv.length < 3 || process.argv.length > 6) {
-    console.log('Usage : node app.js <quantidade> <host> <port> <version>')
+var mineflayer = require('mineflayer')
+const vec3 = require('vec3')
+require("discord.js")
+var config = require('./config.json')
+var estado = "sim"
+var estadokill = "sim"
+var names = require('./names.json')
+if (process.argv.length < 6 || process.argv.length > 6) {
+    console.log('Usage : node app.js <quantity> <host> <port> <version>')
     process.exit(1)
 }
 console.log("MinecraftBOT beta ~ 0.32 made by b1scoito")
 console.log('\x1b[31m',
     "BOT " + '\x1b[0m', "Joining!")
-var seilazao = process.argv[2];
+var seilazao = process.argv[2]
 let i = 0
-var seila2 = Math.floor(Math.random() * names.length);
+var seila2 = Math.floor(Math.random() * names.length)
 
 function next() {
     if (i < seilazao) {
@@ -29,98 +34,139 @@ function createBot(name) {
         host: process.argv[3],
         port: parseInt(process.argv[4]),
         verbose: true
-    });
+    })
     bot.on('login', () => {
         console.log('\x1b[31m',
-            "BOT " + '\x1b[0m' ,
+            "BOT " + '\x1b[0m',
             'LOGIN:  ' + bot.username)
     })
     bot.once('spawn', () => {
-        setInterval(watchTarget, 50)
-
-        function watchTarget() {
-            if (!target) return
-            bot.lookAt(target.position.offset(0, target.height, 0))
-        }
     })
-    bot.on('message', function (msg, _username) {
+    bot.on('message', (message) => {
         console.log('\x1b[31m',
-            "LOG " + '\x1b[32m',
-            bot.username, '\x1b[0m' + " " + msg.toString())
+            "LOG " + '\x1b[29m',
+            bot.username, '\x1b[0m' + " " + message.toAnsi())
     })
     let target = null
     bot.on('chat', function (username, message) {
+        const args = message.slice(config.prefix.length).trim().split(/ +/g)
+        const command = args.shift().toLowerCase()
         if (username === bot.username) return
         target = bot.players[username].entity
-        switch (message) {
+        switch (command) {
             case 'frente':
                 bot.chat("Indo pra frente.")
-                bot.setControlState('left', false)
-                bot.setControlState('back', false)
-                bot.setControlState('right', false)
                 bot.setControlState('forward', true)
-                bot.setControlState('jump', true)
-                break;
+                break
             case 'direita':
                 bot.chat("Indo pra direita.")
-                bot.setControlState('forward', false)
-                bot.setControlState('left', false)
-                bot.setControlState('back', false)
                 bot.setControlState('right', true)
-                bot.setControlState('jump', true)
-                break;
+                break
             case 'esquerda':
                 bot.chat("Indo pra esquerda.")
-                bot.setControlState('forward', false)
-                bot.setControlState('back', false)
-                bot.setControlState('right', false)
                 bot.setControlState('left', true)
-                bot.setControlState('jump', true)
-                break;
+                break
             case 'tras':
                 bot.chat("Indo pra trás.")
-                bot.setControlState('forward', false)
-                bot.setControlState('left', false)
-                bot.setControlState('right', false)
                 bot.setControlState('back', true)
-                bot.setControlState('jump', true)
-                break;
+                break
             case 'parar':
                 bot.chat("Parando todos controles")
                 bot.clearControlStates()
-                break;
-            case 'tp':
-                bot.entity.position.y += 10
+                estado = "nao"
+                estadokill = "nao"
                 break
-            case 'pos':
-                bot.chat(bot.entity.position.toString())
+            case 'tp':
+                if (!args[0]) {
+                    bot.chat("Por favor insira a quantidade de blocos ex: !tp 10")
+                    return
+                }
+                bot.chat("Teleportando para " + args[0] + " blocos.")
+                bot.entity.position.y += parseInt(args[0])
                 break
             case 'seguir':
                 bot.chat("Seguindo.")
-                bot.setControlState('left', false)
-                bot.setControlState('back', false)
-                bot.setControlState('right', false)
+                var doidera = setInterval(watchTarget, 50)
+
+                function watchTarget() {
+                    if (estado === "sim") {
+                        if (!target) return
+                        bot.lookAt(target.position.offset(0, target.height, 0))
+                    } else {
+                        clearInterval(doidera)
+                        estado = "sim"
+                    }
+                }
                 bot.setControlState('forward', true)
-                break;
+                break
             case 'killaura':
-                entity = nearestEntity()
-                if (entity) {
-                    bot.attack(entity, true)
-                } else {
-                    bot.chat('no nearby entities')
+                var doiderao2 = setInterval(atacar, 100)
+
+                function atacar() {
+                    if (estadokill === "sim") {
+                        entity = nearestEntity()
+                        if (entity) {
+                            bot.attack(entity, true)
+                        } else {
+                            bot.chat('no nearby entities')
+                        }
+                    } else {
+                        clearInterval(doiderao2)
+                        estadokill = "sim"
+                    }
                 }
                 break
             case 'pular':
                 bot.setControlState('jump', true)
                 bot.setControlState('jump', false)
                 break
+            case 'lista':
+                sayItems()
+                break
+            case 'minerar':
+                dig()
+                break
+            case 'construir':
+                build()
+                break
+            case 'selecionar':
+                if (!args[0]) {
+                    bot.chat("Por favor insira a quantidade até 8")
+                    return
+                }
+                if (args[0] > 8) {
+                    bot.chat("Valor muito alto")
+                    return
+                } else {
+                    bot.chat("Slot selecionado: " + args[0])
+                    bot.setQuickBarSlot(args[0])
+                }
         }
+    })
+    bot.on('rain', () => {
+        if (bot.isRaining) {
+            bot.chat('Termino de chuve caraio.')
+        } else {
+            bot.chat('Comecou a chuver deixa eu resolver essa trela ae')
+            bot.chat("/weather clear")
+        }
+    })
+
+    bot.on('death', () => {
+        bot.chat('Morri x.x')
     })
     bot.on('end', () => {
         console.log('\x1b[31m',
             "BOT " + '\x1b[32m',
-            bot.username, '\x1b[0m' + " Disconnected")
+            bot.username, '\x1b[0m' + " Disconnected trying to reconnect")
         bot.quit()
+        mineflayer.createBot({
+            version: process.argv[5],
+            username: "botbackup" + i,
+            host: process.argv[3],
+            port: parseInt(process.argv[4]),
+            verbose: true
+        })
     })
 
     function nearestEntity(type) {
@@ -140,5 +186,75 @@ function createBot(name) {
             }
         }
         return best
+    }
+
+    function canSee(pos) {
+        const block = bot.blockAt(pos)
+        const r = bot.canSeeBlock(block)
+        if (r) {
+            bot.chat(`I can see the block of ${block.displayName} at ${pos}`)
+        } else {
+            bot.chat(`I cannot see the block of ${block.displayName} at ${pos}`)
+        }
+    }
+
+    function sayItems(items = bot.inventory.items()) {
+        const output = items.map(itemToString).join(', ')
+        if (output) {
+            bot.chat(output)
+        } else {
+            bot.chat('Nenhum item encontrado.')
+        }
+    }
+
+    function dig() {
+        if (bot.targetDigBlock) {
+            bot.chat(`Ja estou minerando ${bot.targetDigBlock.name}`)
+        } else {
+            var target = bot.blockAt(bot.entity.position.offset(0, -1, 0))
+            if (target && bot.canDigBlock(target)) {
+                bot.chat(`Comecando a minerar ${target.name}`)
+                bot.dig(target, onDiggingCompleted)
+            } else {
+                bot.chat('Não posso minerar')
+            }
+        }
+
+        function onDiggingCompleted(err) {
+            if (err) {
+                console.log(err.stack)
+                return
+            }
+            bot.chat(`Terminei de minerar! ${target.name}`)
+        }
+    }
+
+    function build() {
+        const referenceBlock = bot.blockAt(bot.entity.position.offset(0, -1, 0))
+        const jumpY = bot.entity.position.y + 1.0
+        bot.setControlState('jump', true)
+        bot.on('move', placeIfHighEnough)
+
+        function placeIfHighEnough() {
+            if (bot.entity.position.y > jumpY) {
+                bot.placeBlock(referenceBlock, vec3(0, 1, 0), (err) => {
+                    if (err) {
+                        bot.chat(err.message)
+                        return
+                    }
+                    bot.chat('Coloquei o bloco!')
+                })
+                bot.setControlState('jump', false)
+                bot.removeListener('move', placeIfHighEnough)
+            }
+        }
+    }
+
+    function itemToString(item) {
+        if (item) {
+            return `${item.name} x ${item.count}`
+        } else {
+            return '(nothing)'
+        }
     }
 }
